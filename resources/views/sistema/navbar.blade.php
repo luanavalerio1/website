0d13bc77528c852135aa6360a4fa00567f4d6d76
luanavalerio1 <luanavallerio@gmail.com>
2023-05-16 15:25:08 -0300

    oi

---
+++ resources/views/sistema/navbar.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+    <button class="navbar-toggler" type="button" data.toggle="collpse"
+        data-target="#navbar" aria.controls="navbar" aria-expanded="false"
+        aria-label="Toggle navigation">
+    <span class="navbar-toggle-icon"></span>
